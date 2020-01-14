679043bb273def0f9a73d39e6737a8eabe22c706
김관응 <42456925+kwaneung@users.noreply.github.com>
2020-01-14 17:02:29 +0900

    Update automate_redmine.sh

--- automate_redmine.sh
+++ automate_redmine.sh
@@ -2,8 +2,8 @@
 
 wget https://bitnami.com/redirect/to/818538/bitnami-redmine-4.0.5-6-linux-x64-installer.run
 
-#yum -y install wget perl-Data-Dumper
+yum -y install wget perl-Data-Dumper
 
-#chmod 755 bitnami-redmine-4.0.5-6-linux-x64-installer.run
+chmod 755 bitnami-redmine-4.0.5-6-linux-x64-installer.run
 
-#printf '1\ny\ny\ny\ny\n\n\n\n\n\n\n\nkwaneung\nkwaneung.kim@metanet.co.kr\nmsimamizi\n134652as\n134652as\n' | ./bitnami-redmine-4.0.5-6-linux-x64-installer.run
+printf '1\ny\ny\ny\ny\n\n\n\n\n\n\n\nkwaneung\nkwaneung.kim@metanet.co.kr\nmsimamizi\n134652as\n134652as\n' | ./bitnami-redmine-4.0.5-6-linux-x64-installer.run
