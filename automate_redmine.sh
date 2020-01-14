cb8ab54a1fe1c95271e4db674db0ace63383fb8f
김관응 <42456925+kwaneung@users.noreply.github.com>
2020-01-14 09:33:38 +0900

    Create automate_redmine.sh

---
+++ automate_redmine.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#wget https://bitnami.com/redirect/to/818538/bitnami-redmine-4.0.5-6-linux-x64-installer.run
+
+#yum -y install wget perl-Data-Dumper
+
+#chmod 755 bitnami-redmine-4.0.5-6-linux-x64-installer.run
+
+printf '1\ny\ny\ny\ny\n\n\n\n\n\n\n\nkwaneung\nkwaneung.kim@metanet.co.kr\nmsimamizi\n134652as\n134652as\n' | ./bitnami-redmine-4.0.5-6-linux-x64-installer.run
