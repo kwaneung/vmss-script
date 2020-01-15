e2cb2b43e7d1a611ee2fa208c225d8e8015efb31
김관응 <42456925+kwaneung@users.noreply.github.com>
2020-01-15 11:38:45 +0900

    Update automate_redmine.sh

--- automate_redmine.sh
+++ automate_redmine.sh
@@ -7,3 +7,13 @@ echo 'W3Ucr#f8coyi' | sudo -kS yum -y install wget perl-Data-Dumper
 echo 'W3Ucr#f8coyi' | chmod 755 bitnami-redmine-4.0.5-6-linux-x64-installer.run
 
 printf '1\ny\ny\ny\ny\n\n\n\n\n\n\n\nkwaneung\nkwaneung.kim@metanet.co.kr\nmsimamizi\n134652as\n134652as\n' | ./bitnami-redmine-4.0.5-6-linux-x64-installer.run
+
+cp /opt/redmine-4.0.5-6/ctlscript.sh /etc/init.d/bitnami-redmine
+
+perl -p -i -e '$.==2 and print "# chkconfig: 2345 80 30\n"' /etc/init.d/bitnami-redmine
+perl -p -i -e '$.==3 and print "# description: BitNami Service\n"' /etc/init.d/bitnami-redmine
+perl -p -i -e '$.==4 and print "# processname: bitnami-redmine\n"' /etc/init.d/bitnami-redmine
+perl -p -i -e '$.==2 and print "# Source funtion library\n"' /etc/init.d/bitnami-redmine
+perl -p -i -e '$.==2 and print "./etc/rc.d/init.d/functions\n"' /etc/init.d/bitnami-redmine
+
+chkconfig --add bitnami-redmine
