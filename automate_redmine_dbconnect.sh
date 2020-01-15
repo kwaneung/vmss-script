2e4a2f5d062eff4342a7d7e03cea1df7ea84405d
김관응 <42456925+kwaneung@users.noreply.github.com>
2020-01-15 13:19:53 +0900

    Update automate_redmine_dbconnect.sh

--- automate_redmine_dbconnect.sh
+++ automate_redmine_dbconnect.sh
@@ -1,5 +1,50 @@
 #!/bin/bash
 
-rm -rf /opt/redmine-4.0.5-6/apps/redmine/htdocs/config/database.yml
-touch /opt/redmine-4.0.5-6/apps/redmine/htdocs/config/database.yml
-echo -e 'production:\n  adapter: mysql2\n  database: bitnami_redmine\n  host: 52.231.89.136\n  username: kwan\n  password: gahowumifrag\n  encoding: utf8\n  socket: /opt/redmine-4.0.5-6/mysql/tmp/mysql.sock' > /opt/redmine-4.0.5-6/apps/redmine/htdocs/config/database.yml
+echo '---
+# Default setup is given for MySQL with ruby1.9.
+# Examples for PostgreSQL, SQLite3 and SQL Server can be found at the end.
+# Line indentation must be 2 spaces (no tabs).
+production:
+  adapter: mysql2
+  database: bitnami_redmine
+  host: 52.231.89.136
+  username: kwan
+  password: gahowumifrag
+  encoding: utf8
+  socket: /opt/redmine-4.0.5-6/mysql/tmp/mysql.sock
+development:
+  adapter: mysql2
+  database: redmine_development
+  host: localhost
+  username: root
+  password: ""
+  encoding: utf8
+# Warning: The database defined as "test" will be erased and
+# re-generated from your development database when you run "rake".
+# Do not set this db to the same as development or production.
+test:
+  adapter: mysql2
+  database: redmine_test
+  host: localhost
+  username: root
+  password: ""
+  encoding: utf8
+# PostgreSQL configuration example
+#production:
+#  adapter: postgresql
+#  database: redmine
+#  host: localhost
+#  username: postgres
+#  password: "postgres"
+# SQLite3 configuration example
+#production:
+#  adapter: sqlite3
+#  database: db/redmine.sqlite3
+# SQL Server configuration example
+#production:
+#  adapter: sqlserver
+#  database: redmine
+#  host: localhost
+#  username: jenkins
+#  password: jenkins
+' > database.yml
