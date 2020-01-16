cde2967943ecc1698d6cc5bf04ab830fb747397a
김관응 <42456925+kwaneung@users.noreply.github.com>
2020-01-16 11:00:46 +0900

    Update automate_redmine_plugins.sh

--- automate_redmine_plugins.sh
+++ automate_redmine_plugins.sh
@@ -5,4 +5,9 @@ unzip redmine_agile.zip
 mv redmine_agile /opt/redmine-4.0.5-6/apps/redmine/htdocs/plugins
 cd /opt/redmine-4.0.5-6/apps/redmine/htdocs/plugins
 
-
+perl -p -i -e '$.==9 and print "PATH=/opt/redmine-4.0.5-6/ruby/bin:$PATH:$HOME/bin"' ~/.bash_profile
+source ~/.bash_profile
+yum -y install gcc gcc-c++ httpd-devel
+gem install redmine_crm
+bundle install --no-deployment
+rake redmine:plugins:migrate RAILS_ENV=production
