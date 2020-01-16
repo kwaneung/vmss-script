4dd8317b4562679eaccaf1b221ebb8cade610b30
김관응 <42456925+kwaneung@users.noreply.github.com>
2020-01-16 10:45:28 +0900

    Update automate_redmine_plugins.sh

--- automate_redmine_plugins.sh
+++ automate_redmine_plugins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://raw.githubusercontent.com/kwaneung/vmss-script/master/redmine_agile.zip
-unzip redmine_agile
+unzip redmine_agile.zip
 mv redmine_agile /opt/redmine-4.0.5-6/apps/redmine/htdocs/plugins
 cd /opt/redmine-4.0.5-6/apps/redmine/htdocs/plugins
 
